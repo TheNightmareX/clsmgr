bdc0fe577f57c0a136af375ad52172b2c923c9f4
TheNightmareX <TheNightmareX@foxmail.com>
2020-10-16 19:55:51 +0800

    Optimize element selector. Update UI.

--- students/static/students/js/student_list.js
+++ students/static/students/js/student_list.js
@@ -1,12 +1,17 @@
 import { ActivatorElement } from "../../../../static/public/js/components.js"
 import { setupNumberInput } from "../../../../static/public/js/form.js"
 
+
+/**@type {HTMLDivElement} */
+const $thechosenDialog = document.querySelector('#start-selection-dialog')
 /**@type {HTMLInputElement} */
-const $countInput = document.querySelector('#count-input')
+const $countInput = $thechosenDialog.querySelector('input')
 /**@type {ActivatorElement} */
 const $tableActivator = document.querySelector('#table-activator')
+
 setupNumberInput($countInput, { int: true, min: 0 })
-document.querySelector('#start-btn').addEventListener('click', () => {
+
+$thechosenDialog.addEventListener('confirm.mdui.dialog', () => {
     const count = Number($countInput.value)
     $tableActivator.randomActive(count)
 })
