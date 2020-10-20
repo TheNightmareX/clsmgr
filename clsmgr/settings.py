6743d89e9384d3a1937fbfa9756c9893f75518b6
TheNightmareX <TheNightmareX@foxmail.com>
2020-10-20 19:36:19 +0800

    Seperate django privacy settings.

--- clsmgr/settings.py
+++ clsmgr/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from . import secret_settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm^ec0ml3b@a*g83n=i7th!c1h27o4!2$wbi4ne6@9h8)-n6b1m'
+SECRET_KEY = secret_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
